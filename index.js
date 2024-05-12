f986cabae31b83416a8fd1813978a9f534fc0dfa
Martín Molina <martindmolina@live.com.ar>
2024-05-12 03:04:21 +0000

    code optimization

--- index.js
+++ index.js
@@ -1,49 +1,35 @@
-// index.js
-// where your node app starts
-
-// init project
 var express = require('express');
+var cors = require('cors'); // so that the API is remotely testable by FCC
+
 var app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static('public')); 
 
-// http://expressjs.com/en/starter/basic-routing.html
+// front page
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// get current time JSON
+app.get("/api", (req, res) => {
+  const now = new Date();
+  res.json({unix: now.getTime(), utc: now.toUTCString()});
 });
 
-// My code
-app.get("/api", function (req, res) {
-  res.json({unix: new Date().getTime(), utc: new Date().toUTCString()});
-});
-
-app.get("/api/:date", function (req, res) {
-  var time = req.params.date;
-  var unix, date;
-  if (isNaN(time)) {
-    date = new Date(time);
-    unix = Date.parse(time);
-  }
-  else {
-    unix = parseInt(time);
-    date = new Date(unix);
-  }
-  res.json(date == "Invalid Date" ? {error: "Invalid Date"} : {unix: unix, utc: date.toUTCString()});
+// get time JSON based on a given date or unix
+app.get("/api/:date", (req, res) => {
+  var date = req.params.date;
+  if(!isNaN(date))
+    date = parseInt(date);
+  date = new Date(date);
+  const unix = date.getTime();
+  const utc = date.toUTCString();
+  res.json(date == "Invalid Date" ? {error: "Invalid Date"} : {unix: unix, utc: utc});
 });
 
 // Listen on port set in environment variable or default to 3000
-var listener = app.listen(process.env.PORT || 3000, function () {
+var listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port);
 });
